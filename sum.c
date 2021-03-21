3c67dd3e669e78574d896bb7ce90e2f430584623
Vaneet Prashar <2020a1r164@mietjammu.in>
2021-03-21 22:50:48 +0530

    Files added

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int num1, num2, sum;
+	printf("Enter two numbers\n");
+	scanf("%d %d",&num1, &num2);
+	sum = num1 + num2;
+	printf("Sum of two numbers is: %d",sum);
+	return 0;
+}
