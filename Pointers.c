0f0879560d046cd3f74fee3aef83d383b0708ad8
Pratham <2020a1r164@mietjammu.in>
2021-04-01 19:32:00 +0530

    A basic C Program about pointers.

---
+++ Pointers.c
@@ -0,0 +1,20 @@
+//Author - Pratham Tagotra 
+#include <stdio.h>
+
+int main()
+{
+    int a;  
+    a = 10;
+    int *p = &a;     // declaring and initializing the pointer
+
+    //prints the value of 'a'
+    printf("%d\n", *p);  
+    printf("%d\n", *&a);  
+    //prints the address of 'a'
+    printf("%u\n", &a);    
+    printf("%u\n", p);     
+    
+    printf("%u\n", &p);    //prints address of 'p'
+    
+    return 0;
+}
