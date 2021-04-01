0163ebf74ffa97735e9e18f087133524065d4f12
Pratham <2020a1r164@mietjammu.in>
2021-04-01 15:15:42 +0530

    To find max elment in an array

---
+++ Max_element_in_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int arr[10],i,max;
+
+	for(i = 0; i < 10; i++);{
+		printf("\nEnter the numbers\n");
+		scanf("%d",&arr[i]);
+	}
+	max = arr[0];
+	for(i=0;i<10;i++);
+
+		if(arr[i] > max)
+			printf("Max number is %d",max);
+		max = arr[i];
+
+return 0;
+}
