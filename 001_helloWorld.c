ef04905788fd00e89c07deed24dc1f1803a2c1af
Pratham <2020a1r164@mietjammu.in>
2021-04-05 20:04:16 +0530

    A simple program to print Hello World.

---
+++ 001_helloWorld.c
@@ -0,0 +1,6 @@
+//Author - Pratham Tagotra.
+#include<stdio.h>//Including standard input output headr file.
+int main(){
+	printf("Hello World");//Line to be printed.
+	return 0;//Return statement.
+}
